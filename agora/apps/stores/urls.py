44cde0023fe68f6c141e969f9a9257e0ec33a582
Javier <jachavez6@gmail.com>
2022-06-18 21:05:31 -0400

    Create StoreListView

--- agora/apps/stores/urls.py
+++ agora/apps/stores/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from agora.apps.stores import views
 
 urlpatterns = [
+    path('stores/',
+         views.StoreListView.as_view(),
+         name='store-list'),
     path('new/',
          views.StoreCreateView.as_view(),
          name='store-create'),
--- agora/apps/stores/views.py
+++ agora/apps/stores/views.py
@@ -1,8 +1,23 @@
+from typing import List
 from django.urls import reverse_lazy
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 
 from agora.apps.stores.forms import StoreForm
+from agora.apps.stores.models import Store
+
+
+class StoreListView(ListView):
+    """A view for listing all stores."""
+
+    model = Store
+    context_object_name = 'stores'
+    template_name = 'stores/store_list.html'
+    paginate_by = 25
+    extra_context = {'title': 'Stores'}
+
+    def get_queryset(self):
+        return Store.objects.all()
 
 
 class StoreCreateView(CreateView):
@@ -11,3 +26,4 @@ class StoreCreateView(CreateView):
     form_class = StoreForm
     success_url = reverse_lazy('store-create')
     template_name = 'stores/store_form.html'
+    extra_context = {'title': 'Create Store'}
